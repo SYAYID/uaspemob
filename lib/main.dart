75f0bc6fcb52008a158f0eecd41f011ba8124722
ALatif001 <authenticculuy@gmail.com>
2022-07-18 20:13:56 +0700

    two commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter',
+        title: 'crud UAS Pemob',
         theme: ThemeData(
           accentColor: primaryColor,
         ),
